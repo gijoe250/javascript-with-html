1159e291b4211639c376e89bf37a8d7d455f202e
Joe Garcia <gijoe250@gmail.com>
2022-11-21 13:47:47 -0600

    added new files

--- js/external.js
+++ js/external.js
@@ -1,5 +1,5 @@
 "use strict";
-alert("Welcome to my Website!");
+alert("Welcome to my Website!!!");
 let userAnswer = prompt("What is your favorite color, square?!?");
 console.log("The favorite color is " + userAnswer);
 alert(userAnswer + " is also my favorite color! You are soooooo cool");
