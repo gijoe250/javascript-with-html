8b6799193f9558dac25c1b4773067093b8a37313
Joe Garcia <gijoe250@gmail.com>
2022-11-21 11:20:43 -0600

    add html and js files

---
+++ js/external.js
@@ -0,0 +1,5 @@
+alert("Welcome to my Website!");
+let userAnswer = prompt("What is your favorite color, square?!?");
+console.log("The favorite color is " + userAnswer);
+alert(userAnswer + " is also my favorite color! You are soooooo cool");
+
